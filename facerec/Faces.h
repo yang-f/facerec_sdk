e693966457850ce9d7b803fd9549bdbb86feb916
yangfei <yfei89@126.com>
2017-09-05 17:51:23 +0800

    0.0.5

--- facerec/Faces.h
+++ facerec/Faces.h
@@ -7,8 +7,15 @@
 
 #import <Foundation/Foundation.h>
 #include <vector>
-
+//=result_aflw1w_4w_iter160_far6_notEnd.bin
+//=Cuted_shape_predictor_68_face_landmarks_5_stage.dat
 @interface Faces : NSObject
-+ (std::vector <float*>)recognizeWith:(unsigned char *)data width:(int)width height:(int)height;
+
++ (std::vector <float*>)recognizeWith:(unsigned char *)data width:(int)width height:(int)height angle:(double)angle;
++ (std::vector <float*>)recognizeWith:(unsigned char *)data width:(int)width height:(int)height angle:(double)angle score:(int)score;
++ (NSMutableArray *)pointsWithScaleX:(float)scaleX andScaleY:(float)scaleY;
++ (NSMutableArray *)getAngles;
+
+
 @end
 
